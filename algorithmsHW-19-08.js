942562db88a2e9fe05196210056588073cfabdfc
SerhiiZakorko <129493695+SerhiiZakorko@users.noreply.github.com>
2023-08-24 09:57:57 +0200

    Добавил Ваш вариант

--- algorithmsHW-19-08.js
+++ algorithmsHW-19-08.js
@@ -4,3 +4,25 @@ const arr2 = [72, 86, 113, 119, 265, 445, 892]
 const arr3 = [...arr1, ...arr2]
 const result = arr3.sort((a, b) => a - b)[6]
 console.log(result)
+
+
+function merge(arr1, arr2, k) {
+    let i = 0;
+    let j = 0;
+    let arr = [];
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        arr.push(arr1[i++]);
+      } else {
+        arr.push(arr2[j++]);
+      }
+    }
+    while (i < arr1.length) {
+      arr.push(arr1[i++]);
+    }
+    while (j < arr2.length) {
+      arr.push(arr2[j++]);
+    }
+    return arr[k-1];
+  }
+  console.log(merge([100, 112, 256, 349, 770], [72, 86, 113, 119, 265, 445, 892], 7 ))
